2a682eef9c5e8f0c53dbb397b47df39e88585026
tnt <alkaloid.btx@gmail.com>
2014-09-03 14:05:25 +0200

    Basic rendering

---
+++ engine/Camera.js
@@ -0,0 +1,56 @@
+engine.Camera = function(x, y, width, height, tileSize) {
+	this.position = {
+		x: x, y: y
+	}
+
+	this.width = width;
+	this.height = height;
+	this.tileSize = tileSize;
+}
+
+engine.Camera.prototype.getLeftMost = function() {
+	return this.position.x - (this.width / 2);
+};
+
+engine.Camera.prototype.getLeftMostTile = function() {
+	return Math.floor(this.getLeftMost());
+};
+
+engine.Camera.prototype.getRightMost = function() {
+	return this.position.x + (this.width / 2);
+};
+
+engine.Camera.prototype.getRightMostTile = function() {
+	return Math.floor(this.getRightMost());
+};
+
+engine.Camera.prototype.getTopMost = function() {
+	return this.position.y - (this.height / 2);
+};
+
+engine.Camera.prototype.getTopMostTile = function() {
+	return Math.floor(this.getTopMost());
+};
+
+engine.Camera.prototype.getDownMost = function() {
+	return this.position.y + (this.height / 2);
+};
+
+engine.Camera.prototype.getDownMostTile = function() {
+	return Math.floor(this.getDownMost());
+};
+
+engine.Camera.prototype.xToScreen = function(x) {
+	return (x - this.getLeftMost()) * this.tileSize;
+};
+
+engine.Camera.prototype.yToScreen = function(y) {
+	return (y - this.getTopMost()) * this.tileSize;
+};
+
+engine.Camera.prototype.toScreen = function(pos) {
+	return {
+		x: this.xToScreen(pos.x),
+		y: this.yToScreen(pos.y)
+	}
+}
--- engine/Engine.js
+++ engine/Engine.js
@@ -1,13 +1,77 @@
 var engine = {
+	resolution: {
+		x: null,
+		y: null
+	},
+	tileSize: 16,
+
 	start: function(canvasId, resX, resY) {
-		this.dom.init(canvasId, resX, resY);
+		engine.resolution.x = resX;
+		engine.resolution.y = resY;
 
 		Promise.all([
-			this.resources.init()
+			engine.resourceLoader.javascript("engine/Camera.js")
 		])
 		.then(function() {
-			console.log("done");
+			 return Promise.all([
+					engine.dom.init(canvasId, resX, resY),
+					engine.resources.init(),
+
+					engine.camera.init()
+			]);
+		})
+		.then(function() {
+			requestAnimationFrame(engine.loop);
 		});
+	},
+
+	loop: function() {
+		var lastTime = 0;
+
+		return function(time) {
+			var dt = (time - lastTime) / 1000;
+			lastTime = time;
+
+			engine.update(dt);
+			engine.render();
+		}();
+	},
+
+	update: function(dt) {
+
+	},
+
+	render: function() {
+		var ctx = engine.dom.ctx;
+
+		ctx.save();
+		ctx.fillStyle = "black";
+		ctx.fillRect(0, 0, engine.resolution.x, engine.resolution.y);
+		ctx.restore();
+
+		for(var x = engine.camera.cam.getLeftMostTile(); x < engine.camera.cam.getRightMostTile() + 1; x++) {
+			for(var y = engine.camera.cam.getTopMostTile(); y < engine.camera.cam.getDownMostTile() + 1; y++) {
+				if(engine.resources.map[x] === undefined || engine.resources.map[x][y] === undefined) {
+					continue;
+				}
+
+				var index = engine.resources.map[x][y];
+
+				
+				var clip = engine.resources.mapSpritesheet.indexToPosition(index);
+				var screenPos = engine.camera.cam.toScreen({x: x, y: y});
+
+				ctx.drawImage(engine.resources.mapSpritesheet.img, clip.x, clip.y, engine.tileSize, engine.tileSize, screenPos.x, screenPos.y, engine.tileSize, engine.tileSize);
+			}
+		}
+	}
+};
+
+engine.camera = {
+	cam: null,
+
+	init: function() {
+		this.cam = new engine.Camera(300, 250, engine.resolution.x / engine.tileSize, engine.resolution.y / engine.tileSize, engine.tileSize);
 	}
 };
 
@@ -26,24 +90,33 @@ engine.dom = {
 
 engine.resources = {
 	map: null,
-	mapSpritesheet: null,
+	mapSpritesheet: {
+		img: null,
+
+		indexToPosition: function(index) {
+			var widthInTiles = this.img.width / engine.tileSize;
+
+			var y = Math.floor(index / widthInTiles);
+			var x = index % widthInTiles;
+
+			return {x: x * engine.tileSize, y: y * engine.tileSize};
+		}
+	},
 
 	init: function() {
+		var self = this;
+
 		return Promise.all([
 				engine.resourceLoader.image("resources/spritesheets/tiles.bmp"),
 				engine.resourceLoader.map("resources/maps/map.json")
 		])
 		.then(function(resources) {
 			for(var i = 0; i < resources.length; i++) {
-				var target = null;
-
 				if(resources[i].type === "map") {
-					this.map = resources[i].data;
+					self.map = resources[i].data;
 				} else if(resources[i].type === "img") {
-					target = this.mapSpritesheet;
+					self.mapSpritesheet.img = resources[i].data;
 				}
-
-				target = resources[i].data;
 			}
 		});
 	}
@@ -57,6 +130,18 @@ engine.resourceLoader = {
 				resolve({type: "img", data: img});
 			};
 			img.src = url;
+		});
+		return pr;
+	},
+
+	javascript: function(url) {
+		var pr = new Promise(function(resolve, reject) {
+			var script = document.createElement("script");
+			script.onload = function() {
+				resolve({type: "javascript", data: script});
+			};
+			script.src = url;
+			document.getElementsByTagName("head")[0].appendChild(script);
 		});
 		return pr;
 	},
